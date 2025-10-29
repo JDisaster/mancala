dff6cdf4228f3a6145977ae046351a91a212bfe9
icedhotlatte <kjl001030@gmail.com>
2025-10-29 14:14:06 -0700

    Add src folder and basic Java files

---
+++ src/MancalaMain.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import view.BoardView;
+
+public class MancalaMain {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Mancala Game");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 400);
+        frame.add(new BoardView());
+        frame.setVisible(true);
+    }
+}
